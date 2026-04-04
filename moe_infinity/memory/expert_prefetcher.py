c2ba17f81640ca146e9217bbf0053269d3ec1de1
xly <leyang.xue@ed.ac.uk>
2026-04-04 11:07:41 +0100

    feat(nvtx): add Python NVTX annotations for cache, prefetch, and scheduler paths

--- moe_infinity/memory/expert_prefetcher.py
+++ moe_infinity/memory/expert_prefetcher.py
@@ -4,6 +4,7 @@
 # EfficientMoE Team
 
 
+from contextlib import nullcontext
 from typing import Any, Dict, List, Optional, Set, Tuple
 
 import numpy as np
@@ -11,10 +12,26 @@ from transformers import PretrainedConfig
 
 from moe_infinity.utils import parse_moe_param
 
+try:
+    import nvtx  # type: ignore[reportMissingTypeStubs]
+except ImportError:
+    nvtx = None
+
+HAS_NVTX = nvtx is not None
+
+try:
+    from moe_infinity.profiling.io_profiler import (  # pyright: ignore[reportMissingImports]
+        IOProfiler,
+    )
+except Exception:
+    IOProfiler = None
+
 
 class ExpertPrefetcher(object):
     cache_file_rd: Optional[Any] = None
     first_k_dense_replace: int = 0
+    archer_engine: Any
+    expert_tensor_map: dict[tuple[int, int], int]
 
     def __init__(self, config: PretrainedConfig):
         print(config)
@@ -51,23 +68,40 @@ class ExpertPrefetcher(object):
     def prefetch_experts(self, layer_id: int, expert_matrix):
         if self.archer_engine is None:
             return
-        expert_list = []
-        # print("expert_tensor_map", self.expert_tensor_map)
-        for i in range(layer_id, self.num_layers):
-            for j in range(self.num_experts):
-                if expert_matrix[i, j] > 0:
-                    expert_list.append(
-                        (self.expert_tensor_map[(i, j)], expert_matrix[i, j])
-                    )
-        ordered_expert_list = sorted(
-            expert_list, key=lambda x: x[1], reverse=True
+        profiler = IOProfiler.instance() if IOProfiler is not None else None
+        nvtx_cm = nullcontext()
+        if HAS_NVTX and nvtx is not None:
+            nvtx_cm = nvtx.annotate("prefetch_trigger", color="green")
+        profiler_cm = (
+            profiler.time("prefetch_trigger")
+            if profiler is not None
+            else nullcontext()
         )
-        tensor_ids = [x[0] for x in ordered_expert_list]
-        assert len(np.unique(tensor_ids)) == len(tensor_ids)
-        self.archer_engine.replace_cache_candidates(tensor_ids)
-        for tensor_id in tensor_ids:
-            gpu_id = self.archer_engine.get_node_default_device([tensor_id])
-            self.archer_engine.enqueue_prefetch(tensor_id, gpu_id)
+
+        with profiler_cm:
+            with nvtx_cm:
+                expert_list = []
+                # print("expert_tensor_map", self.expert_tensor_map)
+                for i in range(layer_id, self.num_layers):
+                    for j in range(self.num_experts):
+                        if expert_matrix[i, j] > 0:
+                            expert_list.append(
+                                (
+                                    self.expert_tensor_map[(i, j)],
+                                    expert_matrix[i, j],
+                                )
+                            )
+                ordered_expert_list = sorted(
+                    expert_list, key=lambda x: x[1], reverse=True
+                )
+                tensor_ids = [x[0] for x in ordered_expert_list]
+                assert len(np.unique(tensor_ids)) == len(tensor_ids)
+                self.archer_engine.replace_cache_candidates(tensor_ids)
+                for tensor_id in tensor_ids:
+                    gpu_id = self.archer_engine.get_node_default_device(
+                        [tensor_id]
+                    )
+                    self.archer_engine.enqueue_prefetch(tensor_id, gpu_id)
 
     def speculative_prefetch(
         self, layer_idx: int, router_logits: Optional[Any] = None
